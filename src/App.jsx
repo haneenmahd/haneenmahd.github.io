bf2031f75fdb9dd860aa88deadfbc9338116651d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-28 17:03:01 +0530

    add avatar and basic portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
 import blob from './assets/svg/blob.svg'
 import './App.css'
+import Main from './components/Main'
+import styled from 'styled-components'
+
+const BlobImage = styled.img`
+  height: auto;
+  max-width: 100vw;
+  z-index: -1;
+  position: absolute;
+  top: 0; left: 0;
+`;
 
 function App() {
   return (
     <div className="App">
-      <img src={blob} alt="Background blob" />
+      <BlobImage src={blob} alt="Background blob" />
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import Avatar from "../assets/png/Avatar.png";
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 179px 234px;
+
+    * {
+        margin: 8px 0;
+    }
+`;
+
+const MainAvatar = styled.img`
+    height: 290px;
+    width: 290px;
+    border-radius: 50px;
+`;
+
+const MainHeader = styled.h1`
+    font-style: normal;
+    font-weight: bold;
+    font-size: 48px;
+    line-height: 60px;
+
+    color: #FFFFFF;
+
+    text-shadow: 0px 19px 18px rgba(0, 0, 0, 0.02);
+`;
+
+const SubHeading = styled(MainHeader)`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 28px;
+    line-height: 35px;
+    text-align: center;
+
+    color: #e3e3e3;
+`;
+
+export default function Main() {
+  return (
+    <MainContainer>
+        <MainAvatar src={Avatar} alt="Haneen Mahdin's Avatar" />
+        <MainHeader>Hello, I am Haneen Mahdin</MainHeader>
+        <SubHeading>
+            I am a great hardworker who practices constantly to build and produce great piece of software. I have worked around with Full stack Applications for a long time and I am a OpenSource developer too.
+        </SubHeading>
+    </MainContainer>
+  );
+}
