002c10a834c2e89a2b3913b8f7d486ab22ca9f2b
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-28 17:52:02 +0530

    convert svg icons to jsx

---
+++ src/assets/jsx/Twitter.jsx
@@ -0,0 +1,21 @@
+const Twitter = props => {
+    return <svg width="86" height="79" viewBox="0 0 86 79" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_1_44)">
+<path d="M65.2438 23.2117C65.2793 23.7476 65.2793 24.2836 65.2793 24.8195C65.2793 41.1655 53.7312 60 32.6244 60C26.1218 60 20.0813 57.971 15 54.4493C15.9239 54.564 16.8121 54.6023 17.7716 54.6023C23.137 54.6023 28.0762 52.6501 32.0203 49.3196C26.9746 49.2047 22.7462 45.6446 21.2893 40.7445C22 40.8593 22.7106 40.9359 23.4569 40.9359C24.4873 40.9359 25.5179 40.7827 26.4772 40.5149C21.2183 39.3663 17.274 34.3898 17.274 28.3797V28.2266C18.8019 29.1454 20.5787 29.7196 22.4618 29.796C19.3704 27.5757 17.3451 23.7859 17.3451 19.4984C17.3451 17.2015 17.9135 15.096 18.9085 13.2585C24.5583 20.7617 33.0508 25.6616 42.5735 26.1976C42.3959 25.2788 42.2893 24.3219 42.2893 23.3649C42.2893 16.5507 47.4061 11 53.7665 11C57.071 11 60.0558 12.493 62.1523 14.9047C64.7461 14.3688 67.2334 13.3351 69.4366 11.9188C68.5837 14.7899 66.7715 17.2016 64.3909 18.7328C66.7006 18.4649 68.9391 17.7757 71 16.8188C69.4368 19.2687 67.4824 21.4506 65.2438 23.2117V23.2117Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_1_44" x="0" y="0" width="86" height="79" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="7.5"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_44"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_44" result="shape"/>
+</filter>
+</defs>
+</svg>;
+}
+
+export default Twitter;
