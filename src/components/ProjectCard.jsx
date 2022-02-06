a23215bdd3f48363c30fd05457586247b8307222
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-06 18:29:59 +0530

    add hover effect

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,12 +6,19 @@ const Card = styled.div`
     width: 300px;
     background: #222;
     border-radius: 10px;
-    box-shadow: 0 20px 20px 0 #3f3f3f1f;
+    box-shadow: 0;
+    margin: 20px;
     overflow: hidden;
+    transition: all .3s ;
+    cursor: pointer;
 
     img {
         height: 200px;
         width: 100%;
+    }
+
+    &:hover {
+        box-shadow: 0 30px 60px 0 #6d6d6d1f;
     }
 `;
 
