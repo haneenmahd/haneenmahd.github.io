cca5b751b6d7ba4e5c2569cdc8c6f6713e1a9ca2
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-06 18:30:08 +0530

    use Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import Main from './components/Main'
+import Projects from './pages/Projects'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Main />
+      <Projects />
     </div>
   )
 }
