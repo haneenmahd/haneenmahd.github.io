8130ad754b7d579ddf1ebfddf84ec4a2ec004bf4
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-06 15:28:02 +0530

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import blob from './assets/png/BeachDay.png'
 import './App.css'
-import Main from './components/Main'
-import styled from 'styled-components'
-import Links from './components/Links';
-
-const BlobImage = styled.img`
-  height: auto;
-  width: 100vw;
-  z-index: -1;
-  position: fixed;
-  top: 0; left: 0;
-`;
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <BlobImage src={blob} alt="Background Image" />
-      <Main />
+      <NavBar />
     </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react';
+import styled from "styled-components";
+
+const Container = styled.nav`
+  height: 100px;
+  width: 100vw;
+`;
+
+export default class NavBar extends PureComponent {
+  render() {
+    return (
+      <Container>
+        
+      </Container>
+    );
+  }
+}
