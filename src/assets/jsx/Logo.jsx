fa61a4988753615009bef3302641a5c1845643fb
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-06 15:33:16 +0530

    Change font to mulish

---
+++ src/assets/jsx/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+  min-height: 100%;
+`;
+
+export default function Logo() {
+  return (
+    <LogoContainer>
+      <h3>haneen mahdin</h3>
+    </LogoContainer>
+  );
+}
