67cf7e23ec8f9205712e316334b12d6963561fa8
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-07 07:34:47 +0530

    add titles and description

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Card = styled.div`
-    height: 300px;
+    min-height: 300px;
     width: 300px;
     background: #222;
     border-radius: 10px;
@@ -20,12 +20,34 @@ const Card = styled.div`
     &:hover {
         box-shadow: 0 30px 60px 0 #6d6d6d1f;
     }
+
+    div {
+        margin: 20px 0;
+        padding: 0 0 0 20px;
+    }
+`;
+
+const ProjectTitle = styled.h4`
+    font-size: 130%;
+    font-weight: bold;
+    text-align: left;
+    margin: 0;
+`;
+
+const ProjectDescription = styled.h4`
+    font-size: 100%;
+    font-weight: normal;
+    text-align: left;
+    color: #d1d1d1;
+    margin: 0;
 `;
 
 /**
  * 
  * @param {{
  *   imgSource: string;
+ *   title: string;
+ *   description: string;
  * }} props 
  * @returns 
  */
@@ -33,6 +55,11 @@ export default function ProjectCard(props) {
   return (
       <Card>
           <img src={props.imgSource} alt="Preview Image" />
+          
+          <div>
+              <ProjectTitle>{props.title}</ProjectTitle>
+                <ProjectDescription>{props.description}</ProjectDescription>
+          </div>
       </Card>
   );
 }
