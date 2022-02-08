942eeec409f71afcb85505d2d3ee8b263ad3bb5c
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-08 18:19:14 +0530

    configure theme

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+export default {
+  light: {
+    textColor: "#111",
+    lightText: "#666",
+    navBackground: "rgba(255, 255, 255, 0.5)",
+    cardBackground: "#b3b3b3",
+    linkColor: "#2a2a2a",
+  },
+  dark: {
+    textColor: "#fafafa",
+    lightText: "#c1c1c1",
+    navBackground: "rgba(0, 0, 0, 0.5)",
+    cardBackground: "#222",
+    linkColor: "#d1d1d1",
+  },
+};
