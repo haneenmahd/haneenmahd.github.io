43457f4a0f3a4771a6b10d691bc57749da6cf930
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-08 12:54:37 +0530

    add more pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import Projects from './pages/Projects'
 import NavBar from './components/NavBar'
+import About from './pages/About'
 import { BrowserRouter, Routes, Route, Outlet, useLocation } from 'react-router-dom'
 
 function RenderingApp() {
@@ -29,6 +30,7 @@ ReactDOM.render(
             element={<App />}
           />
           <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
