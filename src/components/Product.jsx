15d49a6cda36ca2a872a8703f49cbeb34cf86b13
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-08 15:30:38 +0530

    Add preview image for products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,29 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Products from '../store/Products.json';
 import { useParams } from 'react-router-dom';
+import WavedPreview from '../assets/img/waved_preview.png';
+
+const TextAnimation = keyframes`
+    0% {
+        opacity: 0.2;
+    }
+
+    50% {
+        opacity: 0.8;
+    }
+
+    100% {
+        opacity: 0.2;
+    }
+`;
+
+const ImageShadowAnimation = keyframes`
+    0% {
+        box-shadow: 0 20px 20px 20px #2400FF34, 0 20px 20px 0 #76197E34;
+    }
+
+    20% {
+        box-shadow: 0 50px 20px 20px #2400FF34, 0 20px 20px 0 #76197E34;
+    }
+    
+    50% {
+        box-shadow: 0 80px 40px 20px #2400FF46, 0 20px 20px 0 #76197E46
+    }
+
+    60% {
+        box-shadow: 0 50px 35px 20px #22c9cc46, 0 20px 20px 0 #76197E34
+    }
+
+    85% {
+        box-shadow: 0 50px 25px 20px #2400FF34, 0 20px 20px 0 #76197E34
+    }
+
+    100% {
+        box-shadow: 0 20px 20px 20px #2400FF34, 0 20px 20px 0 #2222cc34
+    }
+`;
 
 const ProductCard = styled.div`
-  height: 400px;
+  min-height: 100px;
   width: 400px;
   background-color: #222;
   border-radius: 10px;
   padding: 40px 30px;
   text-align: left;
+  overflow: hidden;
   transition: all 0.3s;
 
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
+  * {
     margin: 4px 0;
     padding: 0;
   }
 
   h4 {
     color: #bdbdbd;
+  }
+
+  img {
+    margin-top: 20px;
+    height: auto;
+    max-width: 100%;
+    border-radius: 20px;
+    box-shadow: 0 20px 20px 0 #2400FF76, 0 20px 20px 0 #2222cc76;
+    animation: ${ImageShadowAnimation} 5s linear infinite;
   }
 
   &:hover {
@@ -33,11 +79,11 @@ const ProductCard = styled.div`
 
 export default function Product() {
   const { productName } = useParams();
-  const [productResult, setProductResult] = useState({ productName: "", productDescription: "" });
+  const [productResult, setProductResult] = useState({ productName: "", productDescription: "", previewImageSource: "" });
 
   const findProduct = (name) => {
-    Products.map((product, index) => {
-        if (product.productName === productName) {
+    Products.map((product) => {
+        if (product.productName === name) {
             setProductResult(product);
         }
     });
@@ -51,6 +97,8 @@ export default function Product() {
     <ProductCard>
       <h1>{productResult.productName}</h1>
       <h4>{productResult.productDescription}</h4>
+
+      <img src={productResult.productName === "waved" ? WavedPreview : ""} />
     </ProductCard>
   );
 }
