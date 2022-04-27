9eb90f602edfb85021f4c0e146682362dc7e71aa
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-27 15:29:37 +0530

    Add textfield component

---
+++ src/components/TextField.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const TextFieldStyle = styled.input`
+  height: 30px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #d3d3d3;
+  padding: 10px 20px;
+  width: min-content;
+  max-width: 100%;
+`
+
+const TextField = ({ placeholder = 'Your text field', value, onChange }) => {
+  return (
+    <TextFieldStyle
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
+
+export default TextField
