0d48cacde5a0750920aaeab10585f985176791f9
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-28 12:00:48 +0530

    Add highlighted prop to NavLink

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
 import CommonLink from './CommonLink'
 import theme from '../theme'
@@ -10,14 +10,24 @@ const NavLinkStyle = styled(Link)`
   font-weight: 600;
   text-align: center;
   color: #656d78;
+  padding: 0;
 
   &:hover {
     color: #38393a;
   }
 
-  &:active {
-    color: #38393a;
-  }
+  ${(p) =>
+    p.highlighted &&
+    css`
+      padding: 8px 15px;
+      border-radius: 5px;
+      background-color: #122;
+      color: #fafafa;
+
+      &:hover {
+        color: #ebebeb;
+      }
+    `}
 `
 
 /**
@@ -28,9 +38,13 @@ const NavLinkStyle = styled(Link)`
  * }} props
  * @returns
  */
-export default function NavLink({ to, text }) {
+export default function NavLink({ to, text, highlighted }) {
   return (
-    <NavLinkStyle to={to} aria-label={`Link to ${to} page`}>
+    <NavLinkStyle
+      to={to}
+      aria-label={`Link to ${to} page`}
+      highlighted={highlighted}
+    >
       {text}
     </NavLinkStyle>
   )
