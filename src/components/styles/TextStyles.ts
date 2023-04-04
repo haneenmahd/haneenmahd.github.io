ccfb4df9a29c7509cfa6e27ec461a296c7df9c00
Haneen Mahdin <haneenmahdin@gmail.com>
2023-04-04 08:05:55 +0530

    fix(styles): typography

--- src/components/styles/TextStyles.ts
+++ src/components/styles/TextStyles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import colors from "./ColorStyles"
 
+// NOTE: adjust line height too
+
 export const PageTitle = styled.h1`
     font-weight: 600;
     font-size: 64px;
@@ -9,51 +11,51 @@ export const PageTitle = styled.h1`
 
 export const H1 = styled.h1`
     font-weight: 500;
-    font-size: 40px;
-    line-height: 60px;
+    font-size: 35px;
+    line-height: 50px;
     color: ${colors.black};
 
     @media screen and (max-width: 1024px) {
-        font-size: 36px;
-        line-height: 56px;
+        font-size: 30px;
+        line-height: 45px;
     }
 
     @media screen and (max-width: 600px)  {
-        font-size: 28px;
+        font-size: 25px;
         line-height: 40px;
     }
 `
 
 export const H2 = styled.h2`
     font-weight: 400;
-    font-size: 40px;
-    line-height: 40px;
+    font-size: 23px;
+    line-height: 36px;
     color: ${colors.black};
 
     @media screen and (max-width: 1024px) {
-        font-size: 36px;
-        line-height: 45px;
+        font-size: 20px;
+        line-height: 30px;
     }
 
     @media screen and (max-width: 600px) {
-        font-size: 28px;
-        line-height: 40px;
+        font-size: 18px;
+        line-height: 25px;
     }
 `
 
 export const P = styled.p`
     font-weight: 400;
-    font-size: 24px;
-    line-height: 40px;
+    font-size: 20px;
+    line-height: 30px;
     color: ${colors.gray};
 
     @media screen and (max-width: 1024px) {
-        font-size: 24px;
-        line-height: 40px;
+        font-size: 20px;
+        line-height: 35px;
     }
 
     @media screen and (max-width: 600px) {
-        font-size: 20px;
-        line-height: 40px;
+        font-size: 18px;
+        line-height: 30px;
     }
 `
