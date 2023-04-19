3a5cec3fd1e49d26da8383a859f03b16b80421a3
Haneen Mahdin <haneenmahdin@gmail.com>
2023-04-19 17:00:13 +0530

    feat: add product hunt link

--- src/components/ui/get-in-touch/index.tsx
+++ src/components/ui/get-in-touch/index.tsx
@@ -68,6 +68,19 @@ const GetInTouch: React.FC<GetInTouchProps> = props => {
         <Wrapper>
             <Title className="hidden">Let's get to know each other</Title>
             <Link className="hidden" href="mailto:haneenmahdin@gmail.com">Get in touch</Link>
+
+
+            <a
+                href="https://www.producthunt.com/posts/creativeos?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-creativeos"
+                target="_blank"
+                style={{
+                    marginTop: 30
+                }}>
+                <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=390049&theme=light" alt="CreativeOS - Transform&#0032;the&#0032;way&#0032;you&#0032;manage&#0032;and&#0032;organise&#0032;of&#0032;your&#0032;business | Product Hunt" style={{
+                    width: "100%",
+                    height: 54
+                }} />
+            </a>
         </Wrapper>
     )
 }
